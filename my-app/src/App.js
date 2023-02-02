7e7d7fd96507b76d058a2fdc0d83c8f979afc225
Ant. Lucas Vieira de Lima <lucaslima14072003@gmail.com>
2023-02-02 12:19:51 -0300

    Testes06
    Adicionando porcentagem nas tabelas do gráfico

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,31 @@
 import React from "react";
 import "./App.css";
-import { PieChart,Pie,Tooltip,Cell,Legend } from "recharts";
+import { PieChart, Pie, Tooltip, Cell, Legend } from "recharts";
 
 const App = () => {
     const data = require('./dados.json')
+
+    const RADIAN = Math.PI/180
+    //Calculando o posicionamento dos dados dentro do gráfico
+    const renderLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index, Value}) => {
+      const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+      const x = cx + radius * Math.cos(-midAngle * RADIAN);
+      const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+      return (
+        //Definindo o posicionamento dos valores na frente da label
+        <text
+          key={index}
+          x={x}
+          y={y}
+          fill="white"
+          textAnchor={x > cx ? 'start' : 'end'}
+          dominantBaseline="central"
+        >
+          {`${(percent * 100).toFixed(0)}%`}
+        </text>
+      );
+    };
 
   const COLORS = ['#164212', '#ff0000'];
 
@@ -22,11 +44,13 @@ const App = () => {
             data={data} 
             //cx e cy pra posicionamento
             cx={200}
-            cy={120}
+            cy={100}
             outerRadius={90} 
             fill="#8884d8"
+            //Para não aparecer a linha que indica os números
+            labelLine={false}
             //Mostrar os valores na tabela (Variável Value)
-            label
+            label={renderLabel}
           >
           {data.map((entry, index) => (
                     <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
